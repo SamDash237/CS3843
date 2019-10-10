6197ffbfedb3c2c05e64066bf277f8afca59f5a7
SamDash237 <31454024+SamDash237@users.noreply.github.com>
2019-10-10 02:51:26 -0500

    Add files via upload

---
+++ assign4/calcnew.c
@@ -0,0 +1,3 @@
+int calc(int x, int y, int z) {
+     return 3*x +7*y + 14*z;
+}
---
+++ assign4/testcalcnew.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    int x = 2;
+    int y = 6;
+    int z = 11;
+    int result = calcnew(x,y,z);
+    printf("x=%d, y=%d, z=%d, result = %d\n",x, y, z, result);
+}
